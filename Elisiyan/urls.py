3cc60658aec5fde70ba6cdd9b7b2511c5ac20253
AKIBBB <aaakibmonir@gmail.com>
2024-12-26 05:03:33 +0600

    change

--- Elisiyan/urls.py
+++ Elisiyan/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('product/',include('products.urls')),
     path('users/',include('users.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
