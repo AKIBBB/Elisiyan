cd26dbcb880bece470aa242a65f92c502c668ccb
AKIBBB <aaakibmonir@gmail.com>
2024-12-28 23:48:31 +0600

    url

--- Elisiyan/urls.py
+++ Elisiyan/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/',include('products.urls')),
     path('users/',include('users.urls')),
-]
-
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
