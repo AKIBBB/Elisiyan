46c1b834b92582a6494c29830b01b287a61eb96b
AKIBBB <aaakibmonir@gmail.com>
2025-01-28 01:48:33 +0600

    main

--- Elisiyan/settings.py
+++ Elisiyan/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'products',
     'users',
     'corsheaders',
@@ -52,6 +53,9 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500/',  # Add your frontend domain here
+]
 
 ROOT_URLCONF = 'Elisiyan.urls'
 
